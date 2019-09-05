d8dd5b919c8ca03ab2c79a8d99bb9098ed79ad77
Vitaly Lerner <vitaly@abam2.ls.huji.ac.il>
2019-09-05 14:46:19 +0300

    Compartmentalization: branching and segmentation by 5-10um

--- unit_sim/ptn_sim_neuron.py
+++ unit_sim/ptn_sim_neuron.py
@@ -17,7 +17,7 @@ Geometry: the slices are coronal, from the right hemisphere
 import pandas as pd
 from numpy import *
 from matplotlib.pyplot import *
-
+SEGMENT_LENGTH=5 #micron
 
 class neuron:
 	# parameters are stored as pandas dataframe
@@ -139,29 +139,149 @@ class morphology:
 		
 	def draw(self,params=None):
 		#draw using pyplot library
-		if params==None:
-			style='.k'
-		else:
-			style=params['style']
-		xy0=array(self.swc[:][['x','y']])
-		x0=xy0[:,0]
-		y0=xy0[:,1]
-		plot(x0,y0,style,markersize=.2)
-		axis('equal')
 		
-#print dt.head()
+		if params==None:
+			Layout='Segments'
+		else:
+			Layout=params['Layout']
+			
+		swc=self.swc
+		seg=self.segments
+		if Layout=='Points':
+			xy0=array(swc[:][['x','y']])
+			x0=xy0[:,0]
+			y0=xy0[:,1]
+			plot(x0,y0,'ok',markersize=.3,alpha=0.2)
+			axis('equal')
+		elif Layout=='Branches':
+			
+			for iBranch in range(len(self.branches)):
+				cBr=self.branches.iloc[iBranch,:]
+				cBr_ID=array(cBr['branch_id'])
+
+				cBr_SWC=self.branch_swc(cBr_ID)
+				cBr_x=array(cBr_SWC['x'])
+				cBr_y=array(cBr_SWC['y'])
+				cBr_z=array(cBr_SWC['z'])
+				cBr_r=array(cBr_SWC['r'])
+				plot(cBr_x,cBr_y,'.-',linewidth=0.3,markersize=0.4,alpha=0.8)
+		elif Layout=='Segments':
+			for iSeg in range(len(seg)):
+				cSeg=seg.iloc[iSeg,:]
+				cSeg_start=cSeg['start']
+				cSeg_end=cSeg['end']
+				cSeg_length=cSeg['length']
+				
+				cSeg_start_xy=array(swc[swc['id']==cSeg_start][['x','y']])[0]
+				cSeg_start_x=cSeg_start_xy[0]
+				cSeg_start_y=cSeg_start_xy[1]
+				cSeg_end_xy=array(swc[swc['id']==cSeg_end][['x','y']])[0]
+				cSeg_end_x=cSeg_end_xy[0]
+				cSeg_end_y=cSeg_end_xy[1]
+				plot([cSeg_start_x,cSeg_end_x],[cSeg_start_y,cSeg_end_y],'k',linewidth=cSeg_length/4.)
+				#print cSeg_start_xy,cSeg_end
+	def branch_swc(self,branch_id):
+		swc=self.swc
+		cBr=self.branches[self.branches['branch_id']==branch_id].iloc[0,:]
+		
+		#print cBr
+		cBr_Start=cBr['start']
+		cBr_End=cBr['end']
+		cBr_SWC=swc[ (swc['id']>=cBr_Start) & (swc['id']<=cBr_End)]
+		
+		return cBr_SWC
+	def euclidian_distance(self,p1,p2):
+		return sqrt(sum( (p1-p2)**2))
+		
+	def compartmentalize(self):
+		swc=self.swc
+		br=self.branches
+		Seg_cnt=-1
+		Seg_DF=[]
+		for iBranch in range(len(self.branches)):
+			cBr=self.branches.iloc[iBranch,:]
+			cBr_ID=array(cBr['branch_id'])
+			cBr_SWC=self.branch_swc(cBr_ID)
+			cBr_xyzr=array(cBr_SWC[['x','y','z','r']])
+			cBr_x=cBr_xyzr[:,0]
+			cBr_y=cBr_xyzr[:,1]
+
+			Seg_S=[]
+			Seg_E=[]
+			Seg_D=[]
+			Seg_ID=[]
+			Seg_BID=[]
+			Seg_start_ind=0
+			Seg_cursor=1
+			Seg_end_limit=shape(cBr_xyzr)[0]-1
+			Seg_dist=0
+			while Seg_cursor<Seg_end_limit:
+				while Seg_dist<SEGMENT_LENGTH and Seg_cursor<=Seg_end_limit:
+					p1=cBr_xyzr[Seg_cursor,:3]
+					p2=cBr_xyzr[Seg_cursor-1,:3]
+					loc_dist=self.euclidian_distance(p1,p2)
+					Seg_dist+=loc_dist
+					Seg_cursor+=1
+				Seg_cnt+=1
+				seg_start_ind_global=cBr_SWC.iloc[Seg_start_ind,0]
+				seg_end_ind_global=cBr_SWC.iloc[Seg_cursor-1,0]
+				Seg_S.append(seg_start_ind_global)
+				Seg_E.append(seg_end_ind_global)
+				Seg_D.append(Seg_dist)
+				Seg_BID.append(cBr_ID)
+				Seg_ID.append(Seg_cnt)
+				Seg_dist=0
+				Seg_start_ind=Seg_cursor
+				Seg_cursor+=1
+			Seg_DICT={1:Seg_ID,2:Seg_S,3:Seg_E,4:Seg_D,5:Seg_BID}
+			Seg_Columns=['segment_ID','start','end','length','branch_id']
+			Seg=pd.DataFrame(data=Seg_DICT)
+			Seg.columns=Seg_Columns
+			Seg_DF.append(Seg)
+		self.segments=pd.concat(Seg_DF)
+			
+	def branch(self):
+		#Analyzes swc and finds separate branches
+		#finds their start point parent branches
+		swc=self.swc
+		b0=swc[swc['id']==2]
+		B=swc[swc['pid']!=swc['id']-1][1:]
+		B=pd.concat([b0,B])
+		branch_start=array(B['id'])
+		branch_parent_point=array(B['pid'])
+		
+		branch_end=branch_start[1:]-1
+		branch_end_0=array(swc[-1:]['id'])[0]
+		branch_end=hstack([branch_end,[branch_end_0]])
+
+		branch_id=arange(shape(branch_start)[0])
+		
+		branch_parent_branch=branch_id*0
+		for bID in branch_id:
+			bPP=branch_parent_point[bID]
+			if bPP==1:
+				bBP=-1
+			else:
+				bBP=[k for k in branch_id if k!=bID and bPP>=branch_start[k] and bPP<branch_end[k] ][0]
+			branch_parent_branch[bID]=bBP
+		
+		dt={1:branch_id,2:branch_start,3:branch_end,4:branch_parent_point,5:branch_parent_branch}
+		structure=pd.DataFrame(data=dt)
+		structure.columns=['branch_id','start','end','parent_point','parend_branch']
+		self.branches=structure
+
 
 F=['471129934.swc','515524026.swc','515249852.swc']
 S=[{'style':'.'},{'style':'.'},{'style':'.'}]
-#fSWC='471129934.swc'
-#fSWC='515524026.swc'
-#fSWC='515249852.swc'
-for fSWC,dp,iN in zip(F,S,range(3)):
-	M=morphology(fSWC)
-	
-	M.align()
-	M.translate((100*iN,0,0))
-	M.draw(dp)
+fSWC=F[2]
+
+M=morphology(fSWC)
+M.branch()
+M.compartmentalize()
+M.draw({'Layout':'Points'})
+#M.draw({'Layout':'Branches'})
+#M.draw({'Layout':'Segments'})
+axis('equal')
 show()
 """
 n1=neuron(5)
